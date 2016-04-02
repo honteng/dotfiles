629f85d1ab6dfb31a5af2723319f3d729b1d7648
Naoki Takano <honten@tinkermode.com>
2016-04-02 00:22:22 -0700

    gitconfig

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -3,3 +3,4 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
