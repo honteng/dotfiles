0f569013686f4c31c865eee824d2e65c0d6e6d3e
Naoki Takano <honten@tinkermode.com>
2016-04-08 16:44:20 -0700

    Force yest

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# bash <(curl -s https://raw.githubusercontent.com/honteng/dotfiles/master/setup.sh)
+
+platform='unknown'
+unamestr=`uname`
+if [[ "$unamestr" == 'Linux' ]]; then
+   platform='linux'
+fi
+
+if [[ $platform == 'linux' ]]; then
+  sudo apt-get -y --force-yes install git
+fi
+
 pushd ~
 if [ ! -d dotfiles ]; then
   git clone https://github.com/honteng/dotfiles.git
@@ -10,14 +23,8 @@ git checkout
 git submodule init
 git submodule update --recursive
 
-platform='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-   platform='linux'
-fi
-
 if [[ $platform == 'linux' ]]; then
-   sudo apt-get install zsh
+   sudo apt-get -y --force-yes install zsh
    sudo cp ./linux/peco /usr/local/bin
    sudo chmod a+x /usr/local/bin/peco
 fi
